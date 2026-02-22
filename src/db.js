5168fd2cd7a9148b53c79a71c667a8d21b3a3c45
SiamakSm <siamak036@gmail.com>
2026-02-22 17:36:27 +0100

    Finalize Node.js demo

--- src/db.js
+++ src/db.js
@@ -1,15 +1,15 @@
 // db.js
 // 
 
+require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    host: "localhost",
-    port: 5432,
-    database: "firstp_db",
-    //user: "postgres",
-    // password: "ton_mdp"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
-
 
 module.exports = pool;
